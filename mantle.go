7df65de084367650f3cc5e68f6fb018451dc9474
Viresh <asviresh@gmail.com>
2014-07-29 16:16:57 +0530

    orm defaults to redis

--- mantle.go
+++ mantle.go
@@ -5,15 +5,12 @@ import (
 )
 
 type Mantle interface {
-	//Configure()
-
         Get(key string) string
         Set(key string, value interface{}) bool
         //MGet(key ...interface{}) map[interface{}]interface{}
         //MSet(k_v_map map[interface{}]interface{}) bool
         //Expire(keys ...interface{}) bool
 }
-
 
 type Orm struct {
         Driver string
@@ -22,11 +19,11 @@ type Orm struct {
 }
 
 func (o *Orm) Get() Mantle {
-        if o.Driver == "" {
+        if o.Driver == "memcache" {
+                return Mantle(&mantle.Redis{Host : "", Port : ""})
+        }else{
 		redis := &mantle.Redis{Host : "", Port : ""}
 		redis.Configure()
                 return Mantle(redis)
-        }else{
-                return Mantle(&mantle.Redis{Host : "", Port : ""})
         }
 }
