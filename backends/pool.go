f60cc6194faa611853920233b3b960a7a5f0d1ca
Viresh <asviresh@gmail.com>
2014-09-01 15:36:54 +0530

    bugs

--- backends/pool.go
+++ backends/pool.go
@@ -14,7 +14,6 @@ type PoolSettings struct {
         Timeout time.Duration
 }
 
-//Get a client from pool
 func (rp *ResourcePool) GetConn() (pools.Resource, error) {
         resource, err := rp.pool.Get()
         if err != nil {
@@ -23,24 +22,24 @@ func (rp *ResourcePool) GetConn() (pools.Resource, error) {
         return resource, nil
 }
 
-//Put a client back to pool
 func (rp *ResourcePool) PutConn(conn pools.Resource) {
         rp.pool.Put(conn)
 }
 
-//Redis pool wrapper
 type ResourcePool struct {
         pool *pools.ResourcePool
 }
 
 type dialAndConnect func (host, port string) (pools.Resource, error)
 
-//We create pool using NewPool
+/**
+        We create pool using NewPool
+        dialAndConnect is the callback which creates a connection
+*/
 func NewPool(connect dialAndConnect, settings PoolSettings) *ResourcePool {
         return &ResourcePool{pools.NewResourcePool( newRedisFactory(connect, settings.Host, settings.Port),settings.Capacity,settings.MaxCapacity,settings.Timeout)}
 }
 
-//Helper methods for creating a pool
 func newRedisFactory(connect dialAndConnect, host string, port string) pools.Factory {
         return func() (pools.Resource, error) {
                 return connect(host, port)
