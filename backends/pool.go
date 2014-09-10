9a1acdf9b9dc933df020d0912f00ee0d8dae8d3c
Viresh <asviresh@gmail.com>
2014-09-10 17:14:12 +0530

    more cleaner way to handle host and port in pool

--- backends/pool.go
+++ backends/pool.go
@@ -1,45 +1,44 @@
 package mantle
 
 import (
-        "github.com/youtube/vitess/go/pools"
-        "time"
+	"github.com/youtube/vitess/go/pools"
+	"time"
 )
 
 type PoolSettings struct {
-        Host string
-        Port string
-        Capacity int
-        MaxCapacity int
-        Timeout time.Duration
+	IpAndHosts  []string
+	Capacity    int
+	MaxCapacity int
+	Timeout     time.Duration
 }
 
 func (rp *ResourcePool) GetConn() (pools.Resource, error) {
-        resource, err := rp.pool.Get()
-        if err != nil {
-                return nil, err
-        }
-        return resource, nil
+	resource, err := rp.pool.Get()
+	if err != nil {
+		return nil, err
+	}
+	return resource, nil
 }
 
 func (rp *ResourcePool) PutConn(conn pools.Resource) {
-        rp.pool.Put(conn)
+	rp.pool.Put(conn)
 }
 
 type ResourcePool struct {
-        pool *pools.ResourcePool
+	pool *pools.ResourcePool
 }
 
-type dialAndConnect func (host, port string) (pools.Resource, error)
+//The connect callback that is passed by a backend
+type dialAndConnect func(ipHosts []string) (pools.Resource, error)
 
 func NewPool(connect dialAndConnect, settings PoolSettings) *ResourcePool {
-        return &ResourcePool {
-                        pools.NewResourcePool( newRedisFactory( connect, settings.Host, settings.Port ),
-                        settings.Capacity, settings.MaxCapacity, settings.Timeout )}
+	return &ResourcePool{
+		pools.NewResourcePool(newRedisFactory(connect, settings.IpAndHosts),
+			settings.Capacity, settings.MaxCapacity, settings.Timeout)}
 }
 
-func newRedisFactory(connect dialAndConnect, host string, port string) pools.Factory {
-        return func() (pools.Resource, error) {
-                return connect(host, port)
-        }
+func newRedisFactory(connect dialAndConnect, IpAndHosts []string) pools.Factory {
+	return func() (pools.Resource, error) {
+		return connect(IpAndHosts)
+	}
 }
-
