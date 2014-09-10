cecf4e7c54f9adccdd238fe1a1bd806b8086d98f
Viresh <asviresh@gmail.com>
2014-09-10 13:52:59 +0530

    formatting with Fmt

--- backends/redis.go
+++ backends/redis.go
@@ -1,116 +1,137 @@
 package mantle
 
 import (
-        "github.com/youtube/vitess/go/pools"
-        "github.com/garyburd/redigo/redis"
-        "time"
+	"github.com/garyburd/redigo/redis"
+	"github.com/youtube/vitess/go/pools"
+	"time"
 )
 
 const (
-        PoolSize = 10
-        DefaultHost = "localhost"
-        DefaultPort = "6379"
+	PoolSize    = 10
+	DefaultHost = "localhost"
+	DefaultPort = "6379"
 )
 
 type Redis struct {
-        Settings PoolSettings
-        pool *ResourcePool
+	Settings PoolSettings
+	pool     *ResourcePool
 }
 
 func (r *Redis) SetDefaults() {
-        if r.Settings.Host == "" { r.Settings.Host = DefaultHost }
-        if r.Settings.Port == "" { r.Settings.Port = DefaultPort }
-        if r.Settings.Capacity == 0 { r.Settings.Capacity = PoolSize }
-        if r.Settings.MaxCapacity == 0 { r.Settings.MaxCapacity = PoolSize }
-        r.Settings.Timeout = time.Minute
-        r.pool = NewPool(Connect, r.Settings)
+	if r.Settings.Host == "" {
+		r.Settings.Host = DefaultHost
+	}
+	if r.Settings.Port == "" {
+		r.Settings.Port = DefaultPort
+	}
+	if r.Settings.Capacity == 0 {
+		r.Settings.Capacity = PoolSize
+	}
+	if r.Settings.MaxCapacity == 0 {
+		r.Settings.MaxCapacity = PoolSize
+	}
+	r.Settings.Timeout = time.Minute
+	r.pool = NewPool(Connect, r.Settings)
 }
 
 //Alias to SetDefaults
 func (r *Redis) Configure() {
-        r.SetDefaults()
+	r.SetDefaults()
 }
 
 //Wrapping redis connection
 type RedisConn struct {
-        redis.Conn
+	redis.Conn
 }
 
 //Close a redis connection
 func (r *RedisConn) Close() {
-        _ = r.Conn.Close()
+	_ = r.Conn.Close()
 }
 
 func (r *Redis) GetClient() (*RedisConn, error) {
-        connection, err := r.pool.GetConn()
-        if err != nil{
-                return nil, err
-        }
-        return connection.(*RedisConn), nil
+	connection, err := r.pool.GetConn()
+	if err != nil {
+		return nil, err
+	}
+	return connection.(*RedisConn), nil
 }
 
 func (r *Redis) PutClient(c *RedisConn) {
-        r.pool.PutConn(c)
+	r.pool.PutConn(c)
 }
 
 //This method creates a redis connection
 func Connect(host string, port string) (pools.Resource, error) {
-        cli, err := redis.Dial("tcp", host + ":" + port)
-        if err != nil {
-                panic(err)
-        }
-        return &RedisConn{cli}, nil
+	cli, err := redis.Dial("tcp", host+":"+port)
+	if err != nil {
+		panic(err)
+	}
+	return &RedisConn{cli}, nil
 }
 
 //Generic method to execute any redis call
 func (r *Redis) Execute(cmd string, args ...interface{}) (interface{}, error) {
-        client, err := r.GetClient()
-        if err != nil {
-                return nil, err
-        }
-        defer r.PutClient(client)
-        return client.Do(cmd, args...)
+	client, err := r.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	defer r.PutClient(client)
+	return client.Do(cmd, args...)
 }
 
 func (r *Redis) Delete(keys ...interface{}) int {
-        value, err := redis.Int(r.Execute("DEL", keys...))
-        if err != nil { return -1 }
-        return value
+	value, err := redis.Int(r.Execute("DEL", keys...))
+	if err != nil {
+		return -1
+	}
+	return value
 }
 
 func (r *Redis) Get(key string) string {
-        value, err := redis.String(r.Execute("GET", key))
-        if err != nil { return "" }
-        return value
+	value, err := redis.String(r.Execute("GET", key))
+	if err != nil {
+		return ""
+	}
+	return value
 }
 
 func (r *Redis) Set(key string, value interface{}) bool {
-        _, err := r.Execute("SET", key, value)
-        if err != nil { return false }
-        return true
+	_, err := r.Execute("SET", key, value)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (r *Redis) MGet(keys ...interface{}) []string {
-        values, err := redis.Strings(r.Execute("MGET", keys...))
-        if err != nil { return []string{} }
-        return values
+	values, err := redis.Strings(r.Execute("MGET", keys...))
+	if err != nil {
+		return []string{}
+	}
+	return values
 }
 
 func (r *Redis) MSet(mapOfKeyVal map[string]interface{}) bool {
-        _, err := r.Execute("MSET", redis.Args{}.AddFlat(mapOfKeyVal)...)
-        if err != nil { return false }
-        return true
+	_, err := r.Execute("MSET", redis.Args{}.AddFlat(mapOfKeyVal)...)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (r *Redis) Expire(key string, duration int) bool {
-        _, err := r.Execute("EXPIRE", key, duration)
-        if err != nil { return false }
-        return true
+	_, err := r.Execute("EXPIRE", key, duration)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (r *Redis) Setex(key string, duration int, val interface{}) bool {
-        _, err := r.Execute("SETEX", key, duration, val)
-        if err != nil { return false }
-        return true
+	_, err := r.Execute("SETEX", key, duration, val)
+	if err != nil {
+		return false
+	}
+	return true
 }
-
--- mantle.go
+++ mantle.go
@@ -1,35 +1,35 @@
 package mantle
 
 import (
-        "github.com/vireshas/mantle/backends"
+	"github.com/vireshas/mantle/backends"
 )
 
 type Mantle interface {
-        Get(key string) string
-        Set(key string, value interface{}) bool
-        Delete(keys ...interface{}) int
-        Setex(key string, duration int, value interface{}) bool
-        MGet(key ...interface{}) []string
-        MSet(keyValMap map[string]interface{}) bool
-        Expire(key string, duration int) bool
-        Execute(cmd string, args ...interface{}) (interface{}, error)
+	Get(key string) string
+	Set(key string, value interface{}) bool
+	Delete(keys ...interface{}) int
+	Setex(key string, duration int, value interface{}) bool
+	MGet(keys ...interface{}) []string
+	MSet(keyValMap map[string]interface{}) bool
+	Expire(key string, duration int) bool
+	Execute(cmd string, args ...interface{}) (interface{}, error)
 }
 
 type Orm struct {
-        Driver string
-        Host string
-        Port string
-        Capacity int
+	Driver   string
+	Host     string
+	Port     string
+	Capacity int
 }
 
 func (o *Orm) Get() Mantle {
-        if o.Driver == "memcache" {
-                redis := &mantle.Redis{Settings: mantle.PoolSettings{ Host: o.Host, Port: o.Port, Capacity: o.Capacity }}
+	if o.Driver == "memcache" {
+		redis := &mantle.Redis{Settings: mantle.PoolSettings{Host: o.Host, Port: o.Port, Capacity: o.Capacity}}
 		redis.Configure()
-                return Mantle(redis)
-        }else{
-                redis := &mantle.Redis{Settings: mantle.PoolSettings{ Host: o.Host, Port: o.Port, Capacity: o.Capacity }}
+		return Mantle(redis)
+	} else {
+		redis := &mantle.Redis{Settings: mantle.PoolSettings{Host: o.Host, Port: o.Port, Capacity: o.Capacity}}
 		redis.Configure()
-                return Mantle(redis)
-        }
+		return Mantle(redis)
+	}
 }
