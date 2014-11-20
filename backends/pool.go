5d6359909fc4ad3d661cd6b97f8248aa6122fada
Viresh <asviresh@gmail.com>
2014-11-20 19:59:09 +0530

    documenting code

--- backends/pool.go
+++ backends/pool.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+//This struct has all the generic settings that are required for creating a pool
 type PoolSettings struct {
 	HostAndPorts []string
 	Capacity     int
@@ -13,6 +14,7 @@ type PoolSettings struct {
 	Timeout      time.Duration
 }
 
+//Gets a connection from pool
 func (rp *ResourcePool) GetConn(to time.Duration) (pools.Resource, error) {
 	resource, err := rp.pool.Get(to)
 	if err != nil {
@@ -21,10 +23,12 @@ func (rp *ResourcePool) GetConn(to time.Duration) (pools.Resource, error) {
 	return resource, nil
 }
 
+//Puts a connection back in pool
 func (rp *ResourcePool) PutConn(conn pools.Resource) {
 	rp.pool.Put(conn)
 }
 
+//Wraps pool
 type ResourcePool struct {
 	pool *pools.ResourcePool
 }
@@ -32,12 +36,20 @@ type ResourcePool struct {
 //The connect callback that is passed by a backend
 type dialAndConnect func(instance interface{}) (pools.Resource, error)
 
+/*
+ * Creats an instance of pool
+   Params:
+     connect: A callback which creates a connection to datastore
+     instace: An instance of backend(redis instance)
+     settings: configurations needed to create a pool
+*/
 func NewPool(connect dialAndConnect, instance interface{}, settings PoolSettings) *ResourcePool {
 	return &ResourcePool{
 		pools.NewResourcePool(newRedisFactory(connect, instance),
 			settings.Capacity, settings.MaxCapacity, settings.Timeout)}
 }
 
+//Callback that is passed to NewPool is called here
 func newRedisFactory(connect dialAndConnect, instance interface{}) pools.Factory {
 	return func() (pools.Resource, error) {
 		return connect(instance)
